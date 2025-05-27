47e08eec0ef99422f9a9755d860e59e550d2845c
AkshatMaindola <107314937+AkshatMaindola@users.noreply.github.com>
2025-05-27 17:29:39 +0530

    head.tsx

---
+++ src/app/head.tsx
@@ -0,0 +1,21 @@
+export default function Head() {
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17120831869"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17120831869');
+          `,
+        }}
+      />
+    </>
+  );
+}
